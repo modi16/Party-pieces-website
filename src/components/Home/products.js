9ebad9390145714f19e21de7e0e0d98416b305fc
dimm8187@gmail.com <dimm8187@gmail.com>
2019-07-21 20:40:29 -0400

    product section css

--- src/components/Home/products.js
+++ src/components/Home/products.js
@@ -11,10 +11,46 @@ export default class Products extends Component {
         }
     }
     render() {
+        if (this.state.items.length>0){
         return (
-            <div>
-              content from product  
-            </div>
-        )
+            <section className="menu py-5">
+                <div className="container">
+                    <Title title="Best of products"/>
+                    <div className="row">
+                        {this.state.productItems.map(({node})=>{
+                            return(
+                                <div key={node.id}className="col-11 col-md-6 my-3 d-flex mx-auto">
+                                    <div>
+                                        <Img fixed={node.image.fixed}/>
+                                    </div>
+                                    <div className="flex-grow-1 px-3">
+                                        <div className="d-flex justify-content-between">
+                                            <h6 className="mb-0"><small>{node.title}</small></h6>
+                                            <h6 className="mb-0 text-yellow"><small>${node.price}</small></h6>
+                                        </div>
+                                        <p className="text-muted">
+                                            <small>{node.description.description}</small>
+                                        </p>
+                                    </div>
+
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>           
+            </section>
+        )}
+        else{
+            return(<section className="menu py-5">
+                <div className="container">
+                    <Title title="Best of products"></Title>
+                    <div className="row">
+                        <div className="col-10 col-sm-6 mx-auto text-center text-capitalize">
+                            <h1> There are no products to display</h1>
+                        </div>
+                    </div>
+                </div>
+            </section>)
+        }
     }
 }
